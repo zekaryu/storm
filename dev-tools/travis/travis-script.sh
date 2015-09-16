ffa73f87c04844219d3c172e23f830e066261017
Robert (Bobby) Evans <evans@yahoo-inc.com>
2015-09-16 10:58:29 -0500

    Adressed review comments

--- dev-tools/travis/travis-script.sh
+++ dev-tools/travis/travis-script.sh
@@ -31,7 +31,7 @@ cd ${STORM_SRC_ROOT_DIR}
 export STORM_TEST_TIMEOUT_MS=100000
 
 # We now lean on Travis CI's implicit behavior, ```mvn clean install -DskipTests``` before running script
-mvn test -fae -Pnative -Pstorm-core && mvn test -fae -Pnative -Pstorm-more
+mvn test -fae -Pnative -Pstorm-core && mvn test -fae -Pstorm-more
 BUILD_RET_VAL=$?
 
 for dir in `find . -type d -and -wholename \*/target/\*-reports`;
